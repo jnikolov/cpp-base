5365a82fc5f76393cded51c612ffe92afe37ab6d
dex <dex@enigmah.org>
2017-01-21 03:55:31 +0200

    added conan to the picture

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,13 @@
+#define CATCH_CONFIG_MAIN
 #include "Catch/catch.hpp"
 
-int main(int argc, char* argv[]) 
-{
-    return 0;
+unsigned int Factorial( unsigned int number ) {
+    return number <= 1 ? number : Factorial(number-1)*number;
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
 }
