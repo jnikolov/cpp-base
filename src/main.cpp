d285737d27ab45e9b359c21b1f4de14e8c17a809
dex <dex@enigmah.org>
2017-01-21 05:14:16 +0200

    Testing spdlog

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,14 @@
 
 int main(int argc, char* argv[])
 {
+        spdlog::set_level(spdlog::level::trace);
         auto console = spdlog::stdout_color_mt("console");
+        console->trace("Trace!");
+        console->debug("Debug!");
         console->info("Welcome to spdlog!");
+        console->warn("Warning...!");
         console->error("Some error message with arg{}..", 1);
         console->critical("Omg this is critcal");
-        console->info("Yeyyy It Fakin Works!");
 
         return 0;
 }
