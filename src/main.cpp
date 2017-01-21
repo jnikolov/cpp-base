2a1b2f510cf021d7a0175ef7d2b653b020e81b2e
dex <dex@enigmah.org>
2017-01-21 04:56:19 +0200

    added atom build config file

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "spdlog/spdlog.h"
 
-int main(int argc, char* argv[]) 
+int main(int argc, char* argv[])
 {
-    auto console = spdlog::stdout_color_mt("console");
-    console->info("Welcome to spdlog!");
-    console->error("Some error message with arg{}..", 1);
+        auto console = spdlog::stdout_color_mt("console");
+        console->info("Welcome to spdlog!");
+        console->error("Some error message with arg{}..", 1);
+        console->critical("Omg this is critcal");
+        console->info("Yeyyy It Fakin Works!");
 
-    return 0;
+        return 0;
 }
